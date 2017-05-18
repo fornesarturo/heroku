9e5266d06e05972d1036b7b03de55208121db480
fornesarturo <fornesarturo@gmail.com>
2017-05-18 12:46:30 -0500

    Now with GET and POST

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, request, render_template, send_file
 app = Flask(__name__)
 
 
-@app.route(/sayhi, methods=['POST','GET'])
+@app.route("/sayhi", methods=['POST','GET'])
 def sayingHi():
 	if request.method == 'GET':
 		return "Get this",200
 	if request.method == 'POST':
-		data = request.form
-		return data,200
+		request.get_data()
+		data = request.json
+		string = ""
+		for key in data.keys():
+			string += data[key]
+		return string,200
 
 
