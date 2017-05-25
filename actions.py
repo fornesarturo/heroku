5722abebe20ddf7dadb35725362a1af02b99bafc
fornesarturo <fornesarturo@gmail.com>
2017-05-25 12:48:31 -0500

    Initial convo

---
+++ actions.py
@@ -0,0 +1,28 @@
+def generateAnswer(text):
+    if "hej" in text.lower():
+        return "Hej, jag ar en djur.","text"
+    else:
+        return "Jag ar Skoldpadda.","text"
+
+class EntryManager(object):
+    def __init__(self,entry):
+        self.entry = entry
+        self.message_list = []
+        for event in entry['messaging']:
+            if(event.get("message")):
+                self.message_list.append(event)
+    def answer(self):
+        def getAnswer(event):
+            if event.get("message"):
+                sender = ['sender']['id']
+                text = ['message'].get('text')
+                if not text:
+                    answer,_type = 'Nice ' + str(event['message']['attachments'][0]['type']),'text'
+
+                else:
+                    answer,_type = generateAnswer(text)
+
+                if "text" in _type:
+                    return {'sender':sender,'user_text':text,'text':answer,'type':_type}
+        answer_list = map(getAnswer, self.message_list)
+        return answer_list
--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import json
 from flask import Flask, request, render_template, send_file
+from actions import *
 
 app = Flask(__name__)
+
+PAT = "EAAUXU7pfXaUBANDYYDowVgl631ejD4kARCBmIECs5BqLotsBZCAIZAR6ACUVNJ56QHJelpUZAn4eLYG5QWOZBpR6uF0JtNqtELfv6FqUKRbLlrZC2HIavCgVogFR27k8G93zUnIBjSkRWNNqWO5mZAND2HWtEoHbXrQAoeZBfVB6QZDZD"
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -15,20 +19,41 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def callback():
-	data = request.get_json()
-	if data == None:
-		return request.form
-	if data["object"] == "page":
-		for entry in data["entry"]:
-			answer(entry)
-	return "OK", 200
+    data = request.get_json()
+    if data == None:
+        return request.form
+    if data["object"] == "page":
+        for entry in data["entry"]:
+            EM = EntryManager(entry)
+            result_list = map(answer, EntryManager.answer())
+    return "OK", 200
 
-def answer(entry):
-	for message_event in entry['messaging']:
-		if message_event.get('postback'):
-			return
-		if message_event.get('message'):
-			return
+def answer(answer_details):
+    params  = {"access_token": PAT}
+    headers = {"Content-Type": "application/json"}
+    data    = JSONify(answer_details)
+    if(data is None):
+        return None
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",params=params,headers=headers,data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
+
+#Generate Response
+def JSONify(answer_details):
+    print(answer_details)
+    _type = answer_details['type']
+    sender_id = answer_details['sender']
+
+    if "text" in _type:
+        message_answer = answer_details['text']
+        data = {
+                "recipient":{"id":sender_id},
+                "message":{"text":message_answer}
+                }
+        return json.dumps(data)
+
+#Deal with Entries
 
 @app.route("/sayhi", methods=['POST','GET'])
 def sayingHi():
